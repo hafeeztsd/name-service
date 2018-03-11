49d4ae324451f9fd0eda63b4a851e9a3ac6a29b8
hafeeztsd <hafeeztsd@gmail.com>
2018-03-11 11:44:06 +0400

    Externalized properties for name-service

---
+++ src/main/java/com/mircoservice/nameservice/config/Properties.java
@@ -0,0 +1,29 @@
+package com.mircoservice.nameservice.config;
+
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties("config")
+@RefreshScope
+public class Properties {
+
+	private List<String> names;
+
+	public List<String> getNames() {
+		return names;
+	}
+
+	public void setNames(List<String> names) {
+		this.names = names;
+	}
+
+	@Override
+	public String toString() {
+		return "Properties [names=" + names + "]";
+	}
+
+}
--- src/main/java/com/mircoservice/nameservice/web/NameController.java
+++ src/main/java/com/mircoservice/nameservice/web/NameController.java
@@ -1,21 +1,22 @@
 package com.mircoservice.nameservice.web;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mircoservice.nameservice.config.Properties;
+
 @RestController
 public class NameController {
 
-	private static final List<String> NAMES = Arrays.asList(new String[] { "Abdul Hafeez", "Abdul Sattar",
-			"Muhammed Taha", "Muhammed Talha", "Muhammed Umer", "Muhammed Umair", "Muhammed Haris" });
-
+	@Autowired
+	Properties properties;
+	
 	@RequestMapping(method = RequestMethod.GET, path = "/name")
 	public String getName() {
-		return NAMES.get(new Random().nextInt(NAMES.size() - 1));
+		return properties.getNames().get(new Random().nextInt(properties.getNames().size() - 1));
 	}
 }
